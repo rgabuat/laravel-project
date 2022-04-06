8750eb0195d1a4a8433f884e12aeb46e4aba1bed
rgabuat <johncarlgabuat@gmail.com>
2022-04-06 20:31:56 +0800

    First Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script defer src="https://unpkg.com/alpinejs@3.2.4/dist/cdn.min.js"></script>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
